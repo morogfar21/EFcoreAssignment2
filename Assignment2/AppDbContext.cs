73f39353b51572955e6fa6e9b20a5e0f211d9bea
Henrik T. Jensen <HenrikThueJensen@gmail.com>
2019-10-31 11:15:45 +0100

    Added: Startup

---
+++ Assignment2/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment2
+{
+    class AppDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=Localhost;Initial Catalog=DAB2;Integrated Security=True");
+
+        }
+    }
+}
