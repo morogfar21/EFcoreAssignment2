e9f8bc3b69680c0f1a20b725b552cc7e54751e74
Henrik T. Jensen <HenrikThueJensen@gmail.com>
2019-11-05 08:55:56 +0100

    GuestDish

--- Assignment2/AppDbContext.cs
+++ Assignment2/AppDbContext.cs
@@ -5,11 +5,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Assignment2
 {
-    class AppDbContext: DbContext
+    class AppDbContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=Localhost;Initial Catalog=DAB2;Integrated Security=True");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
 
         }
     }
