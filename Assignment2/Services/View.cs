0a118087b776ec639bf7fb0b00a7cf5fe8d5694a
FrandsAndersen <frands1995@gmail.com>
2019-11-05 11:01:59 +0100

    Added empty View methods

---
+++ Assignment2/Services/View.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2.Services
+{
+    public static class View
+    {
+        //(type) -> list all restaurants with give type and their average rating + latest 5 review text
+        public static void ListRestaurantByType(AppDbContext context)
+        {
+
+        }
+
+        //(restaurant addr) -> menu - dishes, price, avg rating
+        public static void ListRestaurantGeneralInformation(AppDbContext context)
+        {
+
+        }
+
+        //(restaurant addr) -> information about guests reviews for dishes based on table.
+        public static void ListRestaurantBasedOnTableReviews(AppDbContext context)
+        {
+
+        }
+
+
+    }
+}
