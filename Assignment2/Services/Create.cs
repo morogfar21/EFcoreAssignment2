0da339da3d12de3fb3c280c7e6d0219ea9f984fb
Marcus Bech <43644925+morogfar21@users.noreply.github.com>
2019-11-05 10:01:55 +0100

    CreateGuest, Create Person added

--- Assignment2/Services/Create.cs
+++ Assignment2/Services/Create.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Assignment2.Models;
 
 namespace Assignment2.Services
 {
     public static class Create
     {
         #region Marcus
+        /*
+        public static Person CreatePerson (AppDbContext context)
+        {
+            Console.Write("FirstName: ");
+            string firstName = Console.ReadLine();
+
+            Person person = new Person()
+            {
+                Name = firstName,
+            };
+            return person;
+
+            //public List<GuestDish> GuestDishes;
+            // public int Time;
+        }
+        public static Guest CreateGuest(AppDbContext context)
+        {
+            Console.Write("FirstName: ");
+            string firstName = Console.ReadLine();
+
+            Person person = new Person()
+            {
+                Name = firstName,
+            };
+            return person;
+
+            //public List<GuestDish> GuestDishes;
+            // public int Time;
+        }
+        */
         #endregion
 
         #region Bertram
