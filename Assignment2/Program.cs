2fe6af9b278af4eebd0017d28fa446ae9ed57a0d
FrandsAndersen <frands1995@gmail.com>
2019-11-05 10:02:24 +0100

    Added CreateTable

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -15,8 +15,6 @@ namespace Assignment2
             #region Henrik
             #endregion
 
-            #region Frands
-            #endregion
 
             Console.WriteLine("Indtast navnet på restauranten");
             Console.ReadLine();
--- Assignment2/Services/Create.cs
+++ Assignment2/Services/Create.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Assignment2.Models;
 
 namespace Assignment2.Services
 {
@@ -16,6 +17,27 @@ namespace Assignment2.Services
         #endregion
 
         #region Frands
+        public static Table CreateTable(AppDbContext context)
+        {
+            Restaurant rest = FindRestaurant(context);
+
+            Console.Write("Table number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return new Table()
+            {
+                Number = number,
+                Restaurant = rest
+            };
+
+            //[Key]
+            //public int Number { get; set; }
+            //public List<WaiterTable> WaiterTables { get; set; }
+            //[Required]
+            //public Restaurant Restaurant { get; set; }
+            //public List<Guest> Guests { get; set; }
+            
+        }
         #endregion
     }
 }
