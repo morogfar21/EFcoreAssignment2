698cc7c505d82b530473fa91a99620fa1c2feaed
Bertram <bw-sp@live.dk>
2019-11-07 12:31:33 +0100

    Created DummyFunction for Restaurants

--- Assignment2/Services/DummyData.cs
+++ Assignment2/Services/DummyData.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Assignment2.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Assignment2.ShadowModels;
 
 namespace Assignment2.Services
 {
@@ -13,6 +15,33 @@ namespace Assignment2.Services
         #endregion
 
         #region Bertram
+        public void InsertDummyRestaurant(AppDbContext context, string name, string restaurantType, /*string dishType,*/ string address, int numberOfRestaurants)
+        {
+            string[] typeArray = { "Breakfast", "Dinner", "Supper", "Snack" };
+            Random rand = new Random();
+            int randomIndex = rand.Next(typeArray.Length);
+            string dishType = typeArray[randomIndex];
+
+            for (int i = 0; i < numberOfRestaurants; i++)
+            {
+                Restaurant restaurant = new Restaurant()
+                {
+                    Name = name+i,
+                    Type = restaurantType+i,
+                    Address = address+i
+                };
+
+                restaurant.RestaurantDishes = new List<RestaurantDish>()
+                {
+                    new RestaurantDish()
+                    {
+                        RestaurantAddress = address+i,
+                        DishType = dishType
+                    }
+                };
+                context.Restaurants.Add(restaurant);
+            }
+        }
         #endregion
 
         #region Marcus
