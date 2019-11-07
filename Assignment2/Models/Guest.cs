018ea4deac25bd1a803fb648c2b0bc16a0e6ae9f
Marcus Bech <43644925+morogfar21@users.noreply.github.com>
2019-11-07 09:51:04 +0100

    guest changed

--- Assignment2/Models/Guest.cs
+++ Assignment2/Models/Guest.cs
@@ -12,6 +12,9 @@ namespace Assignment2.Models
         public Review Review{ get; set; }
         [Required]
         public int Time { get; set; }
-        public struct DateTime{ }
+
+        public struct DateTime
+        {
+        };
     }
 }
--- Assignment2/Services/Create.cs
+++ Assignment2/Services/Create.cs
@@ -29,12 +29,13 @@ namespace Assignment2.Services
             Console.Write("FirstName: ");
             string firstName = Console.ReadLine();
 
-            DateTime time = DateTime.Now;
-            Console.WriteLine(time);
+            DateTime now = new DateTime();
+            //Console.WriteLine(time);
 
             Guest guest= new Guest()
             {
                 Name = firstName,
+                //Date savenow = DateTime.Now
             };
             return guest;
 
